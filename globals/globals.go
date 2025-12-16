d23ccad686f552d2e217d7cfa00f6b9ed950b738
secretnamebasis <secretnamebasis@keemail.me>
2025-12-16 07:20:43 -0700

    fix: up version

--- globals/globals.go
+++ globals/globals.go
@@ -19,7 +19,7 @@ const MAX_API_VAR_RETURN = 1024
 const FORCE_FASTSYNC_DIFF = int64(100)
 
 // Major.Minor.Patch-Iteration
-var Version = semver.MustParse("3.0.0-alpha.1")
+var Version = semver.MustParse("3.0.0-alpha.2")
 
 // Hardcoded Smart Contracts of DERO Network
 // TODO: Possibly in future we can pull this from derohe codebase
