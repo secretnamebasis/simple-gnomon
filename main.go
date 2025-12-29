b12cbc43cc63024f8dde1795d35c25b1a37e2818
secretnamebasis <secretnamebasis@keemail.me>
2025-12-29 16:55:14 -0700

    fix: correct gui flag orientation

--- main.go
+++ main.go
@@ -15,32 +15,26 @@ func main() {
 
 	if slices.Contains(os.Args, "--help") || slices.Contains(os.Args, "-h") {
 		cmd.Start_gnomon_indexer()
-		fmt.Println(`  --no-gui                          Disable GUI`)
+		fmt.Println(`  --gui                          Enable GUI`)
 		return
 	}
 
 	fmt.Println("Clear is better than clever. \n- Robert Pike")
-	if !slices.Contains(os.Args, "--no-gui") {
-
+	if slices.Contains(os.Args, "--gui") {
 		fmt.Println("GUI ENABLED")
-		app.RenderGUI()
-
-	} else {
-
-		fmt.Println("GUI DISABLED")
-
 		// remove flag from args
-		i := slices.Index(os.Args, "--no-gui")
+		i := slices.Index(os.Args, "--gui")
 		j := i + 1
 		os.Args = slices.Delete(os.Args, i, j)
 
+		app.RenderGUI()
+		// block to prevent gnomon_indexer go routine closure
+		<-exit
+	} else {
 		// start the indexer
 		if err := cmd.Start_gnomon_indexer(); err != nil {
 			fmt.Println(err)
 			return
 		}
-
-		// block to prevent gnomon_indexer go routine closure
-		<-exit
 	}
 }
