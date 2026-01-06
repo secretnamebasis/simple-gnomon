415396a93f9cf5d153ce5218cc9b682d9bb110b0
secretnamebasis <secretnamebasis@keemail.me>
2026-01-06 15:37:47 -0700

    fix: comment out

--- cmd/gnomon.go
+++ cmd/gnomon.go
@@ -420,10 +420,10 @@ func Start_gnomon_indexer() error {
 				if areQueuesEmpty() {
 					time.Sleep(time.Millisecond * time.Duration(longestQueue()))
 				}
-				completion := float64(importable.height) / float64(now)
-				rate := completion * 100
-				to_int := int64(rate)
-				fmt.Printf("\rcompleted %d %%", to_int)
+				// completion := float64(importable.height) / float64(now)
+				// rate := completion * 100
+				// to_int := int64(rate)
+				// fmt.Printf("completed %d %%", to_int)
 				task(importable)
 			}
 		}
