c67e96b30ff41b851ecb969bbc894856e5fd34ea
secretnamebasis <secretnamebasis@keemail.me>
2026-03-06 17:07:22 -0700

    fix: store lowest heights hence forth

--- pkg/handling_heights.go
+++ pkg/handling_heights.go
@@ -26,6 +26,7 @@ func height_handling(ctx context.Context) {
 				return
 			}
 			if achieved_current_height != 0 {
+				storeHeight(lowest_height)
 				time.Sleep((time.Second * time.Duration(info.Target)) / 4) // in quarter time
 			}
 			if ending_height > 0 {
