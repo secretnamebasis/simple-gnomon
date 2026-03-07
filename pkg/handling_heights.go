b3e2ea91dd636968d4eeaf6fade9dae3d9776321
secretnamebasis <secretnamebasis@keemail.me>
2026-03-06 17:57:56 -0700

    fix: clarify last indexed height

--- pkg/handling_heights.go
+++ pkg/handling_heights.go
@@ -26,7 +26,8 @@ func height_handling(ctx context.Context) {
 				return
 			}
 			if achieved_current_height != 0 {
-				storeHeight(achieved_current_height)
+				storeHeight(database, achieved_current_height)
+				storeHeight(backup_database, achieved_current_height)
 				time.Sleep((time.Second * time.Duration(info.Target)) / 4) // in quarter time
 			}
 			if ending_height > 0 {
@@ -74,7 +75,7 @@ func height_handling(ctx context.Context) {
 
 				waitForAllQueues()
 
-				backup(height)
+				backup()
 			}
 		}
 	}
